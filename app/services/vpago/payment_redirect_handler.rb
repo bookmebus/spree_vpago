fbf5308d429793192f71c6f0ac65b30ea3a584fb
engpolyden <polydeneng@gmail.com>
2021-05-25 16:00:15 +0700

    Close #24 add v2 payment api

---
+++ app/services/vpago/payment_redirect_handler.rb
@@ -0,0 +1,62 @@
+require 'faraday'
+
+module Vpago
+  class PaymentRedirectHandler
+    include ActiveModel::Serialization
+
+    attr_accessor :redirect_options, :error_message
+    attr_reader :payment
+
+    delegate      :id, :amount, :response_code, :number, :state,
+                  :payment_method_id, :payment_method_name,
+              to: :payment
+
+    def initialize(payment)
+      @payment = payment
+      @payment_method = @payment.payment_method
+      @payment.process!
+
+      should_process_abapay_deeplink
+      should_process_payway_card
+    end
+
+    def should_process_payway_card
+      return if @payment_method.preferences[:payment_option] != 'cards'
+
+      ## TO DO: generate redirect url
+      data = {
+        href: ''
+      }
+
+      @redirect_options = data
+    end
+
+    def should_process_abapay_deeplink
+      return if @payment_method.preferences[:payment_option] != 'abapay'
+
+      @response = send_process_payment
+
+      if @response.status == 200
+        json_response = JSON.parse(@response.body)
+        
+        if json_response["status"] == "0"
+          @redirect_options = json_response
+        else
+          @error_message = json_response["description"]
+        end
+      end
+    end
+
+    def send_process_payment
+      abapay_payment = ::Vpago::Payway::Checkout.new(@payment)
+      gateway_params = abapay_payment.gateway_params
+      gateway_params[:payment_option] = 'abapay_deeplink'
+
+      conn = Faraday::Connection.new do |faraday|
+        faraday.request  :url_encoded
+      end
+
+     conn.post(abapay_payment.action_url, gateway_params)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Spree::Core::Engine.add_routes do
   # Add your extension routes here
+  namespace :api, defaults: { format: 'json' } do
+    namespace :v2 do
+      namespace :storefront do
+        resources :payments, only: [:show] do
+          resource :payway_cards, only: [:show]
+        end
+      end
+    end
+  end
+
   namespace :webhook do
     resource :payways, only: [] do
       match 'return', to: 'payways#return', via: [:get, :post]
