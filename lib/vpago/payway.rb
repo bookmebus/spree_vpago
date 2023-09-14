7a0a8c0f33bca635e7f0afeb96cbfca589c98420
Thea Choem <theacheng.g6@gmail.com>
2023-09-14 14:41:02 +0700

    close #126 integrate alipay & wechat

--- lib/vpago/payway.rb
+++ lib/vpago/payway.rb
@@ -3,7 +3,9 @@ module Vpago
     CARD_TYPE_ABAPAY = 'abapay'.freeze
     CARD_TYPE_ABAPAY_KHQR = 'abapay_khqr'.freeze
     CARD_TYPE_CARDS = 'cards'.freeze
+    CARD_TYPE_WECHAT = 'wechat'.freeze
+    CARD_TYPE_ALIPAY = 'alipay'.freeze
 
-    CARD_TYPES = [CARD_TYPE_ABAPAY, CARD_TYPE_ABAPAY_KHQR, CARD_TYPE_CARDS].freeze
+    CARD_TYPES = [CARD_TYPE_ABAPAY, CARD_TYPE_ABAPAY_KHQR, CARD_TYPE_CARDS, CARD_TYPE_WECHAT, CARD_TYPE_ALIPAY].freeze
   end
 end
