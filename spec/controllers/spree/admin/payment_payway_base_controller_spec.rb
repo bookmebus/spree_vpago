ded4b93e167b07a5e4a0b07c5d10df2751175adc
panhachom <panhachom@gmail.com>
2024-09-03 18:49:25 +0700

    close #191 recheck order payment status

--- spec/controllers/spree/admin/payment_payway_base_controller_spec.rb
+++ spec/controllers/spree/admin/payment_payway_base_controller_spec.rb
@@ -18,23 +18,10 @@ RSpec.describe Spree::Admin::PaymentPaywayBaseController, type: :controller do
   let(:payment) { create(:payway_payment, payment_method: gateway, source: payment_source, order: order) }
 
   describe "Put update" do
-    it "redirect to with error if order if complated" do
-      allow_any_instance_of(Spree::Order).to receive(:completed?).and_return(true)
-      put :update, params: {id: payment.number}
-
-      expect(flash[:error]).to eq  Spree.t('vpago.payments.not_allow_for_order_completed')
-      expect(response.status).to eq 302
-      expect(response).to redirect_to( admin_order_payment_path(order_id: order.number, id: payment.number) )
-    end
-
     it "passes the validate order" do
       put :update, params: {id: payment.number}
       expect(response.status).to eq 200
       expect(response.body).to eq "ok"
     end
-
   end
-
-
-
 end
