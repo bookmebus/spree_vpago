b3dcb09a495185782251a56a766a1dbcecd20188
Thea Choem <theacheng.g6@gmail.com>
2026-03-23 11:58:37 +0700

    Review mode

--- app/helpers/vpago/vpago_payments_helper.rb
+++ app/helpers/vpago/vpago_payments_helper.rb
@@ -13,6 +13,15 @@ module Vpago
       render partial: 'spree/vpago_payments/transaction_checker'
     end
 
+    # Each payment method may have their own additional processing script,
+    # so we will look for the partial based on the payment method class name.
+    #
+    # eg. processing_scripts/spree/gateway/payway_v2
+    def render_additional_processing_script(payment)
+      processing_script_partial_path = "spree/vpago_payments/processing_scripts/#{payment.payment_method.class.to_s.underscore}"
+      render partial: processing_script_partial_path if lookup_context.exists?(processing_script_partial_path, [], true)
+    end
+
     def mobile_user_agent?
       request.user_agent.to_s.downcase.match?(/android|iphone|ipad|ipod/)
     end
--- lib/spree_vpago/version.rb
+++ lib/spree_vpago/version.rb
@@ -1,7 +1,7 @@
 module SpreeVpago
   module_function
 
-  VERSION = '2.2.2-pre29'.freeze
+  VERSION = '2.2.2-pre33'.freeze
 
   def version
     Gem::Version.new VERSION
